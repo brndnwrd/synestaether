d142dda48c8315029dcc99c98c1336883ca0c51d
Dillon <jdillonh@github.com>
2020-02-04 12:28:39 -0600

    Dillons second commit, I might be about to add abunch of library files I will do my best to delete them

---
+++ Assets/code/Qubit/availableFace.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * This script is for the availableFace prefab object
+ * This object will sit on every available face in the game,
+ * and will allow us and the player to know where they are
+ * placing their next block
+ */
+
+public class availableFace : MonoBehaviour
+{
+    private Collider _collider;
+    private MeshRenderer _renderer;
+    
+    void Start()
+    {
+        _collider = GetComponent<Collider>();
+        _renderer = GetComponent<MeshRenderer>();
+        // you can instead use this to toggle visibility
+        // atleast for debug I'm using colors green/red
+        // _renderer.enabled = false; will make it disappear
+        // ideally we get a wireframe shader material thing, there are free ones around
+        _renderer.material.color = Color.red;
+    }
+
+    void OnMouseEnter()
+    {
+        _renderer.material.color = Color.green;
+    }
+
+    void OnMouseExit()
+    {
+        _renderer.material.color = Color.red;
+    }
+
+    private void OnMouseOver()
+    {
+        _renderer.material.color = Color.green;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
