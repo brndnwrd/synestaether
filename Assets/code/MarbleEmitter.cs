e4b9727f49d3288b16d0b213e15e9b6405a28565
Dillon <jdillonh@github.com>
2020-02-09 17:37:01 -0600

    added models, rigidbodies, redid alot of Editor code, small aesthetic changes

---
+++ Assets/code/MarbleEmitter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MarbleEmitter : MonoBehaviour
+{
+    public GameObject marblePrefab;
+    public float rateOfFire = 0.3f; //in seconds
+    public float lifeTime = 30f;
+    public float initialVelocity = 0f;
+
+    private GameObject marbleparent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating(nameof(Shoot), 2.0f, rateOfFire);
+        marbleparent = GameObject.Find("MarbleParent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Shoot()
+    {
+        GameObject newbie = (GameObject)Instantiate(marblePrefab);
+        newbie.transform.position = gameObject.transform.position;
+        newbie.GetComponent<Rigidbody>().velocity = transform.right * initialVelocity;
+        if (marbleparent != null)
+        {
+            newbie.transform.SetParent(marbleparent.transform);
+        }
+        Destroy(newbie, lifeTime);
+    }
+}
--- Assets/code/Qubit/availableFace.cs
+++ Assets/code/Qubit/availableFace.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using TreeEditor;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 /*
  * This script is for the availableFace prefab object
@@ -15,49 +18,88 @@ public class availableFace : MonoBehaviour
     private Collider _collider;
     private MeshRenderer _renderer;
     private Transform _transform;
+    private Color hoverColor;
+    private Color restColor;
 
     void Start()
     {
         _collider = GetComponent<Collider>();
         _renderer = GetComponent<MeshRenderer>();
         _transform = GetComponent<Transform>();
-        // you can instead use this to toggle visibility
-        // atleast for debug I'm using colors green/red
-        // _renderer.enabled = false; will make it disappear
-        // ideally we get a wireframe shader material thing, there are free ones around
-        _renderer.material.color = Color.red;
+        // DONE: transparency
+        hoverColor = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+        restColor = new Color(0.0f, 0.0f, 1.0f, 0.2f);
+        _renderer.material.color = restColor;
     }
 
     void OnMouseEnter()
     {
-        _renderer.material.color = Color.green;
+        _renderer.material.color = hoverColor;
     }
 
     void OnMouseExit()
     {
-        _renderer.material.color = Color.red;
+        _renderer.material.color = restColor;
     }
 
     private void OnMouseOver()
     {
-        _renderer.material.color = Color.green;
+        _renderer.material.color = hoverColor;
     }
 
     void OnMouseDown()
     {
-        String clicked_object = this.transform.parent.name;
+        
         GameObject editor_object = GameObject.Find("Editor");
-        Editor editor = editor_object.GetComponent <Editor> ();
-        if(clicked_object == "QFloor(Clone)" || clicked_object== "QFloor (1)")
-            editor.PlaceQubit(_transform.position);
-        else if (clicked_object == "QFloor (5)")
-            editor.PlaceQubit(new Vector3(_transform.position.x,_transform.position.y - 5,_transform.position.z - 5));
-        else if (clicked_object == "QFloor (2)")
-            editor.PlaceQubit(new Vector3(_transform.position.x + 5, _transform.position.y - 5, _transform.position.z));
-        else if (clicked_object == "QFloor (3)")
-            editor.PlaceQubit(new Vector3(_transform.position.x - 5, _transform.position.y - 5, _transform.position.z));
-        else if (clicked_object == "QFloor (4)")
-            editor.PlaceQubit(new Vector3(_transform.position.x, _transform.position.y - 5, _transform.position.z + 5));
+        Editor editor = editor_object.GetComponent<Editor>();
+        if( this.transform.parent.name == "QFloor(Clone)" ) {
+            String clicked_object = this.transform.parent.name;
+            if (clicked_object == "QFloor(Clone)" || clicked_object == "QFloor (1)")
+                editor.PlaceQubit(_transform.position + Vector3.up*5f);
+            else if (clicked_object == "QFloor (5)")
+                editor.PlaceQubit(new Vector3(_transform.position.x, _transform.position.y ,
+                    _transform.position.z - 5));
+            else if (clicked_object == "QFloor (2)")
+                editor.PlaceQubit(new Vector3(_transform.position.x + 5, _transform.position.y - 5,
+                    _transform.position.z));
+            else if (clicked_object == "QFloor (3)")
+                editor.PlaceQubit(new Vector3(_transform.position.x - 5, _transform.position.y - 5,
+                    _transform.position.z));
+            else if (clicked_object == "QFloor (4)")
+                editor.PlaceQubit(new Vector3(_transform.position.x, _transform.position.y - 5,
+                    _transform.position.z + 5));
+        }
+        else
+        {
+            Vector3 newPos = transform.position;
+            float dPos = 5f;
+            switch (transform.name)
+            {
+                case "availableFront":
+                    newPos.x += dPos;
+                    break;
+                case "availableBack":
+                    newPos.x -= dPos;
+                    break;
+                case "availableTop":
+                    newPos.y += dPos;
+                    break;
+                case "availableBottom":
+                    newPos.y -= dPos;
+                    break;
+                case "availableSide2":
+                    newPos.z += dPos;
+                    break;
+                case "availableSide1":
+                    newPos.z -= dPos;
+                    break;
+                default:
+                    Debug.LogWarning("Bad Face Name, No Qubit Placed");
+                    return;
+            }
+            editor.PlaceQubit(newPos);
+        }
+
     }
 
     // Update is called once per frame
