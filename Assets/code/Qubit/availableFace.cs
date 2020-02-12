1b0b0126f42e4cd0c7f7042c8a0c0247216f2ac3
Dillon <jdillonh@github.com>
2020-02-12 01:24:26 -0600

    added new QSlant, made block positioning index based

--- Assets/code/Qubit/availableFace.cs
+++ Assets/code/Qubit/availableFace.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using TreeEditor;
 using UnityEngine;
+using UnityEngine.Assertions;
 using Debug = UnityEngine.Debug;
 
 /*
@@ -29,9 +30,15 @@ public class availableFace : MonoBehaviour
         _transform = GetComponent<Transform>();
         // DONE: transparency
         hoverColor = new Color(1.0f, 0.0f, 0.0f, 1.0f);
-        restColor = new Color(0.0f, 0.0f, 1.0f, 0.2f);
+        restColor = new Color(0.0f, 0.0f, 1.0f, 0.0f);
         editColor = new Color(0.0f, 1.0f, 0.0f, 0.4f);
         _renderer.material.color = restColor;
+        if ( transform.parent.name == "QFloor(Clone)" || transform.parent.name == "QFloor")
+        {
+            // this makes floor visible, but other availableFace objects invisible unless hovered
+            restColor.a = 0.1f;
+        } 
+        _renderer.material.color = restColor; 
     }
 
     void OnMouseEnter()
@@ -92,51 +99,60 @@ public class availableFace : MonoBehaviour
         if(state == editState.Create) {
             if (this.transform.parent.name == "QFloor(Clone)")
             {
+                // spa - ghet - EEEE
                 String clicked_object = this.transform.parent.name;
                 if (clicked_object == "QFloor(Clone)" || clicked_object == "QFloor (1)")
-                    editor.PlaceQubit(_transform.position + Vector3.up * 5f);
-                else if (clicked_object == "QFloor (5)")
-                    editor.PlaceQubit(new Vector3(_transform.position.x, _transform.position.y,
-                        _transform.position.z - 5));
-                else if (clicked_object == "QFloor (2)")
-                    editor.PlaceQubit(new Vector3(_transform.position.x + 5, _transform.position.y - 5,
-                        _transform.position.z));
-                else if (clicked_object == "QFloor (3)")
-                    editor.PlaceQubit(new Vector3(_transform.position.x - 5, _transform.position.y - 5,
-                        _transform.position.z));
-                else if (clicked_object == "QFloor (4)")
-                    editor.PlaceQubit(new Vector3(_transform.position.x, _transform.position.y - 5,
-                        _transform.position.z + 5));
+                {
+                    //editor.PlaceQubit(_transform.position + Vector3.up * 5f);
+                    editor.PlaceQubitByIndex(transform.parent.GetComponent<Qubit>().index);
+                }
             }
             else
             {
                 Vector3 newPos = transform.position;
+                Qubit _qubit;
+                if (transform.parent.name == "availableFaces")
+                {  //any Qubit except QFloor
+                    _qubit = transform.parent.parent.gameObject.GetComponent<Qubit>();
+                }
+                else
+                { //QFloor
+                    _qubit = transform.parent.gameObject.GetComponent<Qubit>();
+                }
+                var newIndex = _qubit.index;
                 float dPos = 5f;
                 switch (transform.name)
                 {
                     case "availableFront":
                         newPos.x += dPos;
+                        newIndex.x += 1.0f;
                         break;
                     case "availableBack":
                         newPos.x -= dPos;
+                        newIndex.x -= 1.0f;
                         break;
                     case "availableTop":
                         newPos.y += dPos;
+                        newIndex.y += 1.0f;
                         break;
                     case "availableBottom":
                         newPos.y -= dPos;
+                        newIndex.y -= 1.0f;
                         break;
                     case "availableSide2":
                         newPos.z += dPos;
+                        newIndex.z += 1.0f;
                         break;
                     case "availableSide1":
                         newPos.z -= dPos;
+                        newIndex.z -= 1.0f;
                         break;
                     default:
                         Debug.LogWarning("Bad Face Name, No Qubit Placed");
                         return;
                 }
-                editor.PlaceQubit(newPos);
+                //editor.PlaceQubit(newPos);
+                editor.PlaceQubitByIndex(newIndex);
             }
             _renderer.material.color = restColor;
             //editor.SetState(editState.Edit);
