ba7f6f6fb719c991c29502ea63770099e6b0ec6f
YPPPPan <57540377+YPPPPan@users.noreply.github.com>
2020-02-23 20:57:05 -0600

    Create LevelButton.cs

---
+++ Assets/code/LevelButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Button level;
+    int index;
+    Level QLevel;
+    void Start()
+    {
+        level = this.GetComponent<Button>();
+        level.onClick.AddListener(selectLevel);
+        index = System.Convert.ToInt32(this.GetComponentInChildren<Text>().text);
+        QLevel = GameObject.Find("Level").GetComponent<Level>();
+    }
+
+    void selectLevel()
+    {
+        QLevel.Initialize(index);
+    }
+}
