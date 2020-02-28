06dd82b91f8b60de303275771efd5c2f7741b0f8
YPPPPan <57540377+YPPPPan@users.noreply.github.com>
2020-02-28 14:00:10 -0600

    Update CreateButton.cs

--- Assets/code/CreateButton.cs
+++ Assets/code/CreateButton.cs
@@ -40,8 +40,9 @@ public class CreateButton : Button
         }
         else if (this.name == "Serialize")
         {
-            Serialize save_level = new Serialize();
-            save_level.Save_Level();
+            ResourceMenu res_menu= GameObject.Find("ResourceMenu").GetComponent<ResourceMenu>();
+            res_menu.show();
+            SwitchState();
         }
         else if (this.name == "Level_Menu_Button")
         {
