c86d96a020ba87f52ff85dc5aecbbbe6fb18a658
Dillon <jdillonh@github.com>
2020-03-04 11:54:25 -0600

    added qaccel

---
+++ Assets/code/Qubit/ForceField.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceField : MonoBehaviour
+{
+    public float forceAmount = 1.0f;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        var rb = other.gameObject.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            Debug.Log("collision " + other);
+            rb.AddForce(forceAmount * transform.forward);
+        }
+    }
+    
+}
